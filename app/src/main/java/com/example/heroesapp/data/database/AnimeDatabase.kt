c5c0921f31b00f59a3f442822f81bbe00a0f1d89
Peter-cloud-web <petergichia165@gmail.com>
2023-09-07 11:15:34 +0300

    Define remote key Dao

--- app/src/main/java/com/example/heroesapp/data/database/AnimeDatabase.kt
+++ app/src/main/java/com/example/heroesapp/data/database/AnimeDatabase.kt
@@ -3,10 +3,14 @@ package com.example.heroesapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.heroesapp.data.local.dao.HeroDao
+import com.example.heroesapp.data.local.dao.HeroRemoteKeyDao
 import com.example.heroesapp.domain.model.Hero
+import com.example.heroesapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class,HeroRemoteKey::class], version = 1)
 abstract class AnimeDatabase: RoomDatabase(){
 
     abstract fun heroDao():HeroDao
+
+    abstract fun heroRemoteKeyDao():HeroRemoteKeyDao
 }
---
+++ app/src/main/java/com/example/heroesapp/data/local/dao/HeroRemoteKeyDao.kt
@@ -0,0 +1,21 @@
+package com.example.heroesapp.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.heroesapp.domain.model.HeroRemoteKey
+
+@Dao
+interface HeroRemoteKeyDao {
+
+    @Query("SELECT * FROM hero_remote_key_table WHERE id =:id")
+    suspend fun getRemoteKeyById(id:Int): HeroRemoteKey?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllRemoteKeys(heroRemoteKeys:List<HeroRemoteKey>)
+
+    @Query("DELETE * FROM hero_remote_key_table")
+    suspend fun deleteAllRemoteKeys()
+}
