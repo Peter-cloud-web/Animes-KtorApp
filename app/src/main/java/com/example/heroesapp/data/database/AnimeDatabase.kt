cd545caaf0cb2b8da9c57b553f10160988adc343
Peter-cloud-web <petergichia165@gmail.com>
2023-09-07 12:21:01 +0300

    Add a type converter for our list data

--- app/src/main/java/com/example/heroesapp/data/database/AnimeDatabase.kt
+++ app/src/main/java/com/example/heroesapp/data/database/AnimeDatabase.kt
@@ -2,15 +2,17 @@ package com.example.heroesapp.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.heroesapp.data.DatabaseConverter
 import com.example.heroesapp.data.local.dao.HeroDao
 import com.example.heroesapp.data.local.dao.HeroRemoteKeyDao
 import com.example.heroesapp.domain.model.Hero
 import com.example.heroesapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class,HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class AnimeDatabase: RoomDatabase(){
 
     abstract fun heroDao():HeroDao
-
     abstract fun heroRemoteKeyDao():HeroRemoteKeyDao
 }
